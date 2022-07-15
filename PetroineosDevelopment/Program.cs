472020f59120f92336e1cdcf51379af3c458130a
mowglie <Yash.Patel@yewtechnologies.co.uk>
2022-07-15 17:44:36 +0100

    Read file

---
+++ PetroineosDevelopment/Program.cs
@@ -0,0 +1,11 @@
+﻿using PetroineosDevelopment;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
